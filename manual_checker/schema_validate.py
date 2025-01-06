1b9825e7797d34cf3cb7c99d5aae570ec1031741
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-01-07 01:08:11 +1100

    Better error messages for schema validation of lists

--- manual_checker/schema_validate.py
+++ manual_checker/schema_validate.py
@@ -22,8 +22,8 @@ async def validate_json(schema_table_name, table):
         try:
             jsonschema.validators.validate(instance=table, schema=schema)
         except ValidationError as e:
-            print(f"Validation error for {schema_table_name}: {parseJsonSchemaException(e)}")
-            errors.append(parseJsonSchemaException(e))
+            print(f"Validation error for {schema_table_name}: {parseJsonSchemaException(e, table)}")
+            errors.append(parseJsonSchemaException(e, table))
     return errors
 
 async def download_schema(schema_table_name, url):
@@ -44,14 +44,21 @@ async def download_schema(schema_table_name, url):
         print(f"Invalid schema for {schema_table_name}")
         return False
 
-def parseJsonSchemaException(e: ValidationError) -> str:
-    error = e.message
+def parseJsonSchemaException(e: ValidationError, table: dict | list) -> str:
+    json_path = e.json_path.lstrip('$.')
+    if isinstance(table, list):
+        item = table[e.absolute_path[0]]
+        if "name" in item:
+            json_path = json_path.replace(f'[{e.absolute_path[0]}]', f'[{item["name"]}]')
+    error = f"[{e.validator}] {json_path}: {e.message}"
     if e.validator == 'type':
-        error = f"Type error in the property '{e.json_path.lstrip('$.')}': " + e.message
+        error = f"Type error in the property '{json_path}': " + e.message
     elif e.validator == 'oneOf':
         error = f"At least one of the following properties must be present: {[p['required'] for p in e.validator_value]}"
     elif e.validator == "additionalProperties":
-        error = f"One of your defined property is invalid, it was found at/in '{e.json_path.lstrip('$.')}' and may have unexpected results. \n   Full error: {e.message}"
+        error = f"One of your defined property is invalid, it was found at/in '{json_path}' and may have unexpected results. \n   Full error: {e.message}"
+    elif e.validator == 'minItems':
+        error = f"Minimum number of items not met in '{json_path}': " + e.message
     # elif e.validator == 'required':
     #     error = f"" + e.message
     return error
